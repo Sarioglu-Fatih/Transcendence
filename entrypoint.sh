ab97883ad35cccef5917518dffade9235601f639
Bikinimad <louisrampon@gmail.com>
2024-01-09 15:30:34 +0100

    frontend pour de vrai

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo "Creating Migrations..."
-python project/manage.py makemigrations base_app
+python backend/manage.py makemigrations base_app
 echo ====================================
 
 echo "Starting Migrations..."
-python project/manage.py migrate
+python backend/manage.py migrate
 echo ====================================
 
-python project/manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
+python backend/manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
 
 echo "Starting Server..."
-python project/manage.py runserver 0.0.0.0:8000
+python backend/manage.py runserver 0.0.0.0:8000
 
