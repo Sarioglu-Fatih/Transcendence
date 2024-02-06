0c178ec06d8be98603fa075300f3a8b4c0ccd2c9
Romuald Garcia <rgarcia@z2r6p7.42lyon.fr>
2024-02-06 14:16:49 +0100

    fix merge error

--- backend/frontend/static/js/main.js
+++ backend/frontend/static/js/main.js
@@ -217,6 +217,13 @@ window.uploadAvatar = async function () {
 	handleAvatarUpload()
 }
 
+// Link avatar to homepage
+const baseURL = window.location.protocol + '//' + window.location.hostname + (window.location.port ? ':' + window.location.port : '');
+const homePageURL = '/home';
+const fullHomePageURL = baseURL + homePageURL;
+const homelink = document.getElementById('homelink');
+homelink.href = fullHomePageURL;
+
 const authButton = document.getElementById('authButton');
 authButton.addEventListener('click', () => {
   function generateRandomState() {
@@ -239,12 +246,4 @@ authButton.addEventListener('click', () => {
 });
 
 export { isUserLoggedIn, state }
-// Link avatar to homepage
-const baseURL = window.location.protocol + '//' + window.location.hostname + (window.location.port ? ':' + window.location.port : '');
-const homePageURL = '/home';
-const fullHomePageURL = baseURL + homePageURL;
-const homelink = document.getElementById('homelink');
-homelink.href = fullHomePageURL;
-
-export { isUserLoggedIn}
  
