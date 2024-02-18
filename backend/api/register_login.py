d6f81457a0549fa9bf455d5f223ed525745d8bb8
ElMama11 <mverger@z2r7p4.42lyon.fr>
2024-02-18 18:14:11 +0100

    limit username fixed

--- backend/api/register_login.py
+++ backend/api/register_login.py
@@ -59,7 +59,7 @@ def user_login(request):
 	data = json.loads(request.body.decode('utf-8'))
 	username = data.get('username')
 	password = data.get('password')
-	regexUsername = r'^[a-zA-Z0-9_-]+$'																# login page parsing
+	regexUsername = r'^[a-zA-Z0-9_-]{1,16}$'															# login page parsing
 	if (not re.match(regexUsername, username)):
 		return JsonResponse({'error': 'Username not valide'})
 	user = authenticate(request, username=username, password=password)
